ff0750a1eb5523c356e22c9257cfc8975920733c
Abhijeet <109912987+DeadLoss1801@users.noreply.github.com>
2024-03-03 19:38:10 +0530

    Writing testcases for Escrow.sol  in escrow.js

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,13 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
+    address public lender;
+    address public inspector;
+    address payable public seller;
+    address public nftAddress;
 
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        lender = _lender;
+        inspector = _inspector;
+        nftAddress = _nftAddress;
+        seller = _seller;
+    }
 }
